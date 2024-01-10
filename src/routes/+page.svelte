7d365b2faeed417bef9c8678c4f5a7c3d5d5828d
morde <mordechai.hadad01@gmail.com>
2024-01-10 13:20:16 +0200

    add: color picker cool

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,17 @@
-<script>
+<script lang="ts">
 	import ColorButton from '$lib/components/ColorButton.svelte';
 	import Navbar from '$lib/components/Navbar.svelte';
 	import namedColors from 'color-name-list';
 	import Values from 'values.js';
 
 	let colorValue = '#ffffff';
-	let levelsValue = 10;
+	let levelsValue: number = 10;
 	let colorName = 'White';
 	let color = new Values(colorValue);
 	let shades = color.shades(levelsValue);
 	let tints = color.tints(levelsValue).reverse();
+	let colorPicker: HTMLInputElement;
+	let colorPickerValue = '';
 
 	$: {
 		shades = color.shades(levelsValue);
@@ -21,20 +23,35 @@
 	<Navbar></Navbar>
 	<div class="flex items-center justify-center gap-4">
 		<button
-			class="material-symbols-outlined color-box rounded-md bg-white px-9 py-7 text-neutral-700"
+			class="material-symbols-outlined color-box rounded-md {color.getBrightness() <= 50
+				? 'text-neutral-300'
+				: 'text-neutral-700'} h-full px-9 py-7"
+			style="background-color: {color.hexString()};"
+			on:click={() => colorPicker.click()}
 		>
 			colorize
 		</button>
+		<input
+			type="color"
+			bind:this={colorPicker}
+			class="hidden"
+			bind:value={colorPickerValue}
+			on:change={(e) => {
+				const v = e?.target?.value;
+				colorValue = v;
+				color = new Values(v);
+			}}
+		/>
 		<div class="flex flex-col gap-3 text-neutral-900">
 			<p class="text-sm">{colorName}</p>
 			<input
-				class="w-min text-xl"
+				class="w-min p-1 text-xl"
 				bind:value={colorValue}
-				size="3"
+				size="5"
 				type="text"
 				maxlength="7"
-				on:input={() =>
-					(colorValue = namedColors.find((x) => x.hex === colorValue)?.name ?? colorName)}
+				on:input={(e) =>
+					(color = e?.target?.value.length === 7 ? new Values(e?.target?.value) : color)}
 				on:keydown={(e) => {
 					if (e.key === 'Enter') {
 						color = new Values(colorValue);
@@ -48,11 +65,14 @@
 					maxlength="3"
 					type="text"
 					bind:value={levelsValue}
+					on:input={(e) => {
+						levelsValue = Number(e?.target?.value);
+					}}
 				/>
 			</p>
 		</div>
 	</div>
-	<div class="grid grid-cols-3 justify-center gap-3">
+	<div class="grid grid-cols-3 justify-center gap-3 md:grid-cols-6">
 		{#each tints as tint}
 			<ColorButton color={tint} weight={tint.weight}></ColorButton>
 		{/each}
